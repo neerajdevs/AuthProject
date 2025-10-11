cabe4e5d8f63ee8545bfb2faffe8a3596c87f25e
neerajdevs <neerajgupta150019@gmail.com>
2025-10-11 11:08:35 +0530

    Day 1st

--- authsystem/settings.py
+++ authsystem/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'authsystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
